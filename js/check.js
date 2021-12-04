31af98b4dcf93ac582b808a6794246762c239c5e
kucold <52961133+kucold@users.noreply.github.com>
2021-12-04 17:17:52 +0700

    Delete js directory

--- js/check.js
+++
@@ -1,7 +0,0 @@
-$(".politick").click(function() {
-    if (($(this).prop("checked"))) {
-      $("#form__button").removeAttr("disabled");
-    } else {
-      $("#form__button").attr("disabled", "disabled");
-    }
-  })
--- js/nav.js
+++
@@ -1,5 +0,0 @@
-let downButton = document.querySelector('.down-button');
-
-downButton.onclick = function () {
-  window.scrollTo(100, 100);
-};
